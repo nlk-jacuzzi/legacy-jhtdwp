48446c84faf24267b73c5e04a19952a582c2de7d
alex chousmith <alex@ninthlink.com>
2013-01-28 23:44:18 +0000

    fix missing phone # in sidebar

--- sidebar.php
+++ sidebar.php
@@ -13,9 +13,18 @@
  */
  
 $options = get_option('progo_options');
+$locs = get_posts( array(
+			'numberposts'	=> -1,
+			'post_type'		=> 'progo_loc',
+			'orderby'		=> 'menu_order',
+			'order'			=> 'ASC'
+		));
+if ( count($locs) > 0 ) {
+	$loc = get_post_meta($locs[0]->ID, '_progo_loc', true);
 ?>
-<div class="block calltoday"><h2><strong>CALL TODAY :</strong> <?php echo esc_attr($options['businessphone']); ?></h2></div>
+<div class="block calltoday"><h2><strong>CALL TODAY :</strong> <?php echo esc_attr($loc['businessphone']); ?></h2></div>
 <?php
+}
 wp_nav_menu( array( 'container' => false, 'theme_location' => 'mainmenu', 'menu_id' => 'mainmenu', 'fallback_cb' => 'progo_nomenu_cb' ) );
 
 $sidebar = 'main';
